e32f0507401e4bebcd284d198d24d7f93f80a367
developedbyjose <josegeraldlumbao0307@gmail.com>
2025-06-09 19:07:15 +0800

    feat: add MongoDB database connection setup
    - Add MongoDB connection configuration
    - Implement environment variable handling
    - Add error handling for database connection
    - Configure environment-specific connection settings

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import { PORT } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
@@ -18,8 +19,10 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  // connect to database
+  await connectToDatabase();
 });
 
 export default app;
